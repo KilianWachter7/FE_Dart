1ac5436f54205cd066f7d8b1b6cd57b4de8cc686
KilianWachter7 <kilian.wachter@web.de>
2023-04-18 21:37:36 +0200

    fix: app dienstag

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import { Route, Redirect, RouteComponentProps } from 'react-router-dom'
 import { IonRouterOutlet, IonPage, IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, IonButton } from '@ionic/react'
 import { home, apps, settings, gameController } from 'ionicons/icons'
 import { Home } from './pages/Home'
-import { Settings } from './pages/Settings'
-import { BackendCall } from './pages/BackendCall'
+import { Settings } from './pages/Sonstiges/Settings'
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css'
@@ -33,7 +32,6 @@ export const App: React.FunctionComponent<RouteComponentProps> = props => {
         <IonRouterOutlet>
           <Route path="/home" component={Home} exact />
           <Route path="/about" component={Settings} exact />
-          <Route path="/backend" component={BackendCall} exact />
           <Redirect exact from="/" to="/home" />
         </IonRouterOutlet>
         
@@ -45,10 +43,6 @@ export const App: React.FunctionComponent<RouteComponentProps> = props => {
           <IonTabButton tab="about" href="/about">
             <IonIcon icon={settings} />
             <IonLabel>Settings</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="backend" href="/backend">
-            <IonIcon icon={settings} />
-            <IonLabel>Backend</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
