c9a42fb6db877a4e91498a9d73004a43afcef257
KilianWachter7 <kilian.wachter@web.de>
2023-05-08 16:32:48 +0200

    fix: hostname in webpack angepasst

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,12 +1,31 @@
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
+const { networkInterfaces } = require("os");
+
+const nets = networkInterfaces();
+const results = Object.create(null); // Or just '{}', an empty object
+
+for (const name of Object.keys(nets)) {
+  for (const net of nets[name]) {
+    // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+    // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
+    const familyV4Value = typeof net.family === "string" ? "IPv4" : 4;
+    if (net.family === familyV4Value && !net.internal) {
+      if (!results[name]) {
+        results[name] = [];
+      }
+      results[name].push(net.address);
+    }
+  }
+}
+console.log("Hostname:", results["en0"][0]);
+const hostname = results["en0"][0];
 
 module.exports = merge(common, {
   mode: "development",
   devtool: "inline-source-map",
   devServer: {
-    // host: "MacBook-Air-von-Kilian.local",
-    host: "192.168.0.214",
+    host: hostname,
 
     historyApiFallback: {
       index: "/",
